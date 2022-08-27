acfe17bb19d63f1917a7a4b038c950ce4d98ef91
rajeshk <rajezvlb@gmail.com>
2022-08-27 13:15:22 +0530

    class 1

---
+++ session1.py
@@ -0,0 +1,28 @@
+
+# Keywords - Reserved words
+
+# Identifiers - variables
+
+
+# 1. Should not be the keyword
+# 2. Should hold alphaNumeric character
+# 3. Underscore is allowed at starting but not recomended
+# 4. Should not start with the number
+# 5. Whitespace not alllowed
+# 6. snake_casing is the structure
+# 7. To mark the variable as constant, initialize the variable name in ALL CAPS
+
+
+# Varibles
+
+# All Identifiers are varaibles until it obeys the above rules
+
+# Statements
+# Every piece of code is a statement
+
+# Adding 1 and two
+# a= 1+2
+
+# Comments
+
+# Some piece of code that never executes when the program is running
