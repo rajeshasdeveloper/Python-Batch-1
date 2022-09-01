6f0f5ef7d3ba0f987efadf798eee8d0edab9f95a
jothiprakash <jothiprakashnatarajan@gmail.com>
2022-09-01 11:19:54 -0700

    find the max and min number

---
+++ session_2/workout problems/jothiprakash/maxmin.py
@@ -0,0 +1,13 @@
+j=[12,34,45,23,1]
+a=j[1]
+b=j[1]
+max=0
+min=0
+for i in j:
+    if i>a:
+        max+=i
+if i<b:
+    min+=i
+print("The maximum number is",max)
+print("The minimum number is",min)
+
--- session_2/workout problems/jothiprakash/reversethestring.py
+++ session_2/workout problems/jothiprakash/reversethestring.py
@@ -2,6 +2,6 @@ str="sreejith"
 str1=''
 j= len(str)-1
 while j>=0:
-    str1+=str[j]
+    str1+=str[j] 
     j-=1
 print(str1)
