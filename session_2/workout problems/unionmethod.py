d9edab2c280eaeb6ab9c5b6c4d5668532a8f7a6b
jothiprakash <jothiprakashnatarajan@gmail.com>
2022-09-01 12:13:30 -0700

    union of two array

---
+++ session_2/workout problems/unionmethod.py
@@ -0,0 +1,14 @@
+a=[1,2,3,4,5,6]
+b=[3,4,5,6,7,78]
+c=[]
+for i in a :
+    c.append(i)
+for j in b:
+    if j not in  c:
+        c.append(j)
+print("union of a and b is",c)
+    
+
+    
+
+      
