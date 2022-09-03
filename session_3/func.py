fd66544e86f7a24301422db8b2bab6a98f63387f
rajeshk <rajezvlb@gmail.com>
2022-09-03 21:24:33 +0530

    session_3

---
+++ session_3/func.py
@@ -0,0 +1,48 @@
+# def sum_of_arrays(arr):
+#     n = arr[-1]
+#     return n*(n+1)/2
+
+
+# lst=list(range(1000000000))
+# sum_res = sum_of_arrays(lst)
+# print(sum_res)
+
+# # Time complexity - O(1)
+# # Space complecity - O(n)
+
+# for iterator in iterable
+
+# while condtion
+
+# while i <= 10:
+#     # logical block
+#     # incremental statement
+#     i += 1
+
+
+# if 1 == 1:
+#     pass
+# elif 2==2:
+#     pass
+# elif 2==2:
+#     pass
+# elif 2==2:
+#     pass
+# elif 2==2:
+#     pass
+# else:
+#     pass
+
+# special keywords
+
+# pass
+# continue
+# break
+
+
+# for j in range(6):
+#     for i in range(6):
+#         if i != 4:
+#             print('I am', j, i)
+#         else:
+#             break
