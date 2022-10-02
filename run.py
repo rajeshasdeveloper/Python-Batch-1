02d4ef86a059866935bca7e481d78bb75133de4a
rajeshk <rajezvlb@gmail.com>
2022-10-02 11:29:36 +0530

    added .gitignore

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ from waitress import serve
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=4444)
-    # serve(app, host="0.0.0.0", port=4444)
-    gunicorn_app = app()
+    serve(app, host="0.0.0.0", port=4444)
 else:
     gunicorn_app = app()
